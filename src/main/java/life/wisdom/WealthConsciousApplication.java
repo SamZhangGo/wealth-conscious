07c4368339c45275636fbdf96885cb00ff8b0756
samzhanggo <googling22@gmail.com>
2020-04-16 21:29:20 +0800

    init commit

---
+++ src/main/java/life/wisdom/WealthConsciousApplication.java
@@ -0,0 +1,13 @@
+package life.wisdom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WealthConsciousApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WealthConsciousApplication.class, args);
+	}
+
+}
---
+++ src/test/java/life/wisdom/WealthConsciousApplicationTests.java
@@ -0,0 +1,13 @@
+package life.wisdom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WealthConsciousApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
